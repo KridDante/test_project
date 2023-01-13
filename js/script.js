5d63a3a085e191a60ba97dddd58d384023982f01
KridDante <kriddante@gmail.com>
2023-01-13 22:53:04 +0200

    New

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let helloWorld = "Hello world"
+console.log(helloWorld)
+
+let firstNumber = 20
+let secondNumber = 10
+
+let result = firstNumber + secondNumber
+console.log(result)
+
+let resultOne = firstNumber 
+
+
+let pi = parseFloat("3.14")
+let zda = parseInt("3.14")
+
+console.log(pi)
+console.log(zda)
+
+let piNumber = secondNumber +  pi
+
+console.log(piNumber)       
